3dcd02bfe6a155729918b3561b2b6ffb8022fad5
Ibrohim Ikromzoda <ibrohim.ikromzoda@gmail.com>
2019-12-07 01:04:47 -0700

    added new classes

--- src/day59_Exceptions/NoBreakTimeException.java
+++ src/day59_Exceptions/NoBreakTimeException.java
@@ -1,11 +1,9 @@
 package day59_Exceptions;
-
-public class NoBreakTimeException extends RuntimeException {
-	
-	public NoBreakTimeException() {
-		
-		super("Too much break have occured in java course");
-		System.out.println("There is no break till we get job done");
-	}
-
+public class NoBreakTimeException extends RuntimeException{
+    public NoBreakTimeException() {
+        
+        super("Too much breaks have occured in java course");
+        System.err.println("There is no break till we get job done");
+    }
+    
 }
